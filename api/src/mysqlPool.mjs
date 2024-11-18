44759ee0020ffe7d31dc45942575ffbaf08102f2
Leonid Pipchenkov <t70a80@gmail.com>
2024-11-18 11:10:51 +0300

    Initialize

---
+++ api/src/mysqlPool.mjs
@@ -0,0 +1,30 @@
+import mysql from 'mysql2'
+
+const pool = mysql.createPool({
+  connectionLimit: 100,
+  host: 'mysql',
+  port: '3306',
+  user: 'root',
+  password: 'password',
+  database: 'time_db',
+})
+
+const CREATE_TIMES_TABLE_SQL = `CREATE TABLE IF NOT EXISTS times (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    time TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)`
+
+pool.getConnection((err, connection) => {
+  if (!err) {
+    console.log('Connected to the MySQL DB - ID is ' + connection.threadId)
+    connection.query(CREATE_TIMES_TABLE_SQL, (err) => {
+      if (!err) {
+        console.log('Times table was created')
+      }
+    })
+    connection.release()
+  }
+})
+
+export default pool
